4f5f84444f1399c44d058571a2c6e7339292d4be
BogdanTarchenko <tarchenko.bogdan@outlook.com>
2026-02-26 01:00:40 +0700

    Initial Commit

---
+++ InternetBanking/InternetBankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternetBankingApp.swift
+//  InternetBanking
+//
+//  Created by Богдан Тарченко on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct InternetBankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
