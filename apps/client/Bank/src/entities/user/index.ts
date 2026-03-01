cc6f3bde76c1758ab98f3ce1b219ca05935238f2
unknown <vladislaw.2000.pro@gmail.com>
2026-03-01 17:13:38 +0700

    add register form

--- apps/client/Bank/src/entities/user/index.ts
+++ apps/client/Bank/src/entities/user/index.ts
@@ -1,4 +1,4 @@
 export type { User, AuthTokens, UserPage, UserRole } from './model/types'
-export { LoginSchema, CreateUserSchema } from './model/schema'
+export { LoginSchema, RegisterSchema, CreateUserSchema } from './model/schema'
 export type { LoginInput, RegisterInput, CreateUserInput, EditUserInput } from './model/schema'
 export { UserApi } from './api/user.api'
